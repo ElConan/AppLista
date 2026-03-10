6d716c4df1245bad5a6b975be53a0c74dddae805
ElConan <jhoelnico213@hotmail.com>
2026-03-09 19:16:21 -0500

    Implementacion para trabajar con SQLite

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace AppLista
+﻿using AppLista.Services;
+
+namespace AppLista
 {
     public partial class App : Application
     {
@@ -7,6 +9,22 @@
             InitializeComponent();
 
             MainPage = new AppShell();
+        }
+
+        static DBnota database;
+
+        public static DBnota Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    string path_db = Path.Combine(FileSystem.AppDataDirectory, "notas.db");
+                    database = new DBnota(path_db);
+                }
+
+                return database;
+            }
         }
     }
 }
---
+++ ViewModels/NotaViewModel.cs
@@ -0,0 +1,33 @@
+﻿using AppLista.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLista.ViewModels
+{
+    public class NotesViewModel : BaseViewModel
+    {
+        public ObservableCollection<Nota> Notas { get; set; } = new();
+
+        public Command cargar_notascommand { get; }
+
+        public NotesViewModel()
+        {
+            cargar_notascommand = new Command(async () => await cargar_notas());
+        }
+
+        async Task cargar_notas()
+        {
+            var notas = await App.Database.get_notas();
+            Notas.Clear();
+
+            foreach (var nota in notas)
+            {
+                Notas.Add(nota);
+            }
+        }
+    }
+}
